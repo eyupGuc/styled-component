116bb01126f722d926d92b51a7fec68badac5d4a
eyupGuc <meydanguresi@gmail.com>
2022-11-02 03:38:36 +0300

    footer

--- src/component/styles/Footer.styled.jsx
+++ src/component/styles/Footer.styled.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const StyleFooter=styled.div`
-background-color:#8a1c4a ;
-
+background-color:${({theme})=>theme.colors.footer} ;
+color: #fff;
+padding: 100px 0;
 `;
