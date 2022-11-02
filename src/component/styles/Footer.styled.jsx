964bc6d0c7bbc1aa71c542c0082314a78475e406
eyupGuc <meydanguresi@gmail.com>
2022-11-02 03:42:33 +0300

    move

--- src/component/styles/Footer.styled.jsx
+++ src/component/styles/Footer.styled.jsx
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 
-export const StyleFooter=styled.div`
-background-color:${({theme})=>theme.colors.footer} ;
-color: #fff;
-padding: 100px 0;
+export const StyleFooter = styled.div`
+  background-color: ${({ theme }) => theme.colors.footer};
+  color: #fff;
+  padding: 100px 0;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    text-align: center;
+    ul {
+      padding: 0;
+    }
+    p {
+      text-align: center;
+    }
+  }
 `;
